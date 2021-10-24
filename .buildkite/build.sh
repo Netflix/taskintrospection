3701e8351b537ad51e7f90bea1a1603ac8b250cc
Sargun Dhillon <sargun@sargun.me>
2021-10-24 01:22:49 -0700

    Fix build

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+VERSION=$(git describe --tags)
+
+cat > dkms.conf <<EOF
+PACKAGE_NAME="taskintrospection"
+PACKAGE_VERSION="${VERSION}"
+CLEAN="make clean"
+MAKE[0]="make -C \${kernel_source_dir} M=\${dkms_tree}/\${PACKAGE_NAME}/\${PACKAGE_VERSION}/build"
+CLEAN="make -C \${kernel_source_dir} M=\${dkms_tree}/\${PACKAGE_NAME}/\${PACKAGE_VERSION}/build clean"
+BUILT_MODULE_NAME[0]="taskintrospection"
+DEST_MODULE_LOCATION[0]="/updates"
+AUTOINSTALL="yes"
+EOF
+
+mkdir -p dkms
+mkdir -p src
+dkms add . --dkmstree $PWD/dkms --sourcetree $PWD/src
+dkms mkdeb -m taskintrospection -v 0.1 --dkmstree $PWD/dkms --sourcetree $PWD/src
 
