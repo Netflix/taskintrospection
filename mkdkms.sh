f27ce9929c5282bf8718c0003360eee29922c3d9
Sargun Dhillon <sargun@sargun.me>
2021-10-25 18:51:08 -0700

    Fix DKMS.sh

--- mkdkms.sh
+++ mkdkms.sh
@@ -9,6 +9,7 @@ CLEAN="make clean"
 MAKE[0]="make -C \${kernel_source_dir} M=\${dkms_tree}/\${PACKAGE_NAME}/\${PACKAGE_VERSION}/build"
 CLEAN="make -C \${kernel_source_dir} M=\${dkms_tree}/\${PACKAGE_NAME}/\${PACKAGE_VERSION}/build clean"
 BUILT_MODULE_NAME[0]="taskintrospection"
+DEST_MODULE_LOCATION[0]="/extra"
 AUTOINSTALL="yes"
 EOF
 
